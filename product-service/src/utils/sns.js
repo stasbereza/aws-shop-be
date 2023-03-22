a4abe60e425869165a9378ce763ce2b692f4f5ce
Stanislav <stanislav.bereza@spglobal.com>
2023-03-22 21:01:58 +0200

    refactor sendNotification function

--- product-service/src/utils/sns.js
+++ product-service/src/utils/sns.js
@@ -3,19 +3,18 @@ import AWS from "aws-sdk";
 const sns = new AWS.SNS({ region: process.env.REGION });
 
 export const sendNotification = async (product) => {
+  const params = {
+    Subject: "Product creation notification",
+    Message: `Product ${product.title} is created successfully!`,
+    MessageAttributes: {
+      price: { DataType: "Number", StringValue: product.price },
+    },
+    TopicArn: process.env.SNS_ARN,
+  };
+
   await sns
-    .publish(
-      {
-        Subject: "Product creation notification",
-        Message: `Product ${product.title} is created successfully!`,
-        MessageAttributes: {
-          price: { DataType: "Number", StringValue: product.price },
-        },
-        TopicArn: process.env.SNS_ARN,
-      },
-      () => {
-        console.log("Notification sent!");
-      }
-    )
+    .publish(params, () => {
+      console.log("Notification sent!");
+    })
     .promise();
 };
